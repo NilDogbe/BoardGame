fc8b505f5386cf59341a4a18e762ee887447a264
nil dogbe <nil.d75@hotmail.com>
2018-12-20 15:31:04 +0200

    test

---
+++ cmake-build-debug/CMakeFiles/BoardGame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BoardGame.dir/main.cpp.obj"
+  "BoardGame.pdb"
+  "BoardGame.exe"
+  "BoardGame.exe.manifest"
+  "libBoardGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BoardGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/BoardGame.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BoardGame.
+# This may be replaced when dependencies are built.

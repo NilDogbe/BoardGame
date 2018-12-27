de8ee8b8c395cc2db72d213b7771c7439097a31f
Nil <nil.d75@hotmail.com>
2018-12-27 19:55:49 +0200

    Suppression de bugg (cin) a durant le start

--- Game.cpp
+++ Game.cpp
@@ -46,12 +46,13 @@ bool Game::movePiece(int x_dep, int y_dep, int x_arr, int y_arr) {
 
     // cout << (piece_dep->canMove(x_dep, y_dep, x_arr, y_arr) == 2) << "eeeeeee";
 
-    if (x_dep >= m_size || x_dep < 0 || y_dep >= m_size || y_dep < 0 || x_arr >= m_size || x_arr < 0 || y_arr >= m_size || y_arr < 0 )  // hors des  limites ?
+    if (x_dep >= m_size || x_dep < 0 || y_dep >= m_size || y_dep < 0 || x_arr >= m_size || x_arr < 0 ||
+        y_arr >= m_size || y_arr < 0)  // hors des  limites ?
         //  cout << "G1" << endl;
         return false;
-        Piece *piece_dep = m_board.at(y_dep * m_size + x_dep);
-        Piece *piece_arr = m_board.at(y_arr * m_size + x_arr);
-     if (piece_dep == nullptr) // piece existe
+    Piece *piece_dep = m_board.at(y_dep * m_size + x_dep);
+    Piece *piece_arr = m_board.at(y_arr * m_size + x_arr);
+    if (piece_dep == nullptr) // piece existe
         return false;
     else if (piece_dep->canMove(x_dep, y_dep, x_arr, y_arr) == 0) // piece existe
         return false;
@@ -63,7 +64,7 @@ bool Game::movePiece(int x_dep, int y_dep, int x_arr, int y_arr) {
 
 void Game::getPossibleMove(int current_x, int current_y) {
     int nb_move = 0;
-   // cout<< "ca vas dedans" <<endl;
+    // cout<< "ca vas dedans" <<endl;
     Piece *current_piece = m_board.at(current_y * m_size + current_x);
     if (current_piece != nullptr) {
         vector<int> res = current_piece->getPossibleMoves(current_x, current_y);
@@ -71,7 +72,7 @@ void Game::getPossibleMove(int current_x, int current_y) {
         for (int i = 0; i < res.size(); i++) {
             int tmp = res.at(i);
             int x = tmp % m_size;
-            int y = tmp/m_size;
+            int y = tmp / m_size;
             /* cout <<"tmp = " << tmp;
              cout <<"x = " << x;
              cout <<"y = " << y << endl;*/
@@ -80,12 +81,12 @@ void Game::getPossibleMove(int current_x, int current_y) {
                 nb_move++;
             }
         }
-        cout<<"Il y a "<<nb_move<<" move possible."<<endl;
     }
+    cout << "Il y a " << nb_move << " move possible." << endl;
 }
 
 void Game::move(int x_dep, int y_dep, int x_arr, int y_arr) {
-    cout<<"Dans move " << m_name<<endl;
+    cout << "Dans move " << m_name << endl;
     if (movePiece(x_dep, y_dep, x_arr, y_arr)) {
         Piece *piece_dep = m_board.at(y_dep * m_size + x_dep);
         Piece *piece_arr = m_board.at(y_arr * m_size + x_arr);
@@ -93,27 +94,26 @@ void Game::move(int x_dep, int y_dep, int x_arr, int y_arr) {
             if (piece_dep->toString() == "P")
                 ((PawnForChess *) piece_dep)->setFirstMove();
             if (piece_arr != nullptr && piece_arr->toString().compare("K") == 0) {
-                cout<<"aaaaaaaaaaaaaaa";
+                cout << "aaaaaaaaaaaaaaa";
                 m_endGame = true;
                 return;
             }
             m_board.at(y_dep * m_size + x_dep) = nullptr;
             m_board.at(y_arr * m_size + x_arr) = piece_dep;
-        }
-        else if(m_name.compare(Game::GAME_DAME) == 0){
-           vector<int> travel = piece_dep->getTravel();
-           for(int i = 0;i<travel.size();i++){
-               if(m_board.at(travel.at(i)) != nullptr){
-                   cout<<"Les dames ca fait peur"<<endl;
-                   m_board.at(travel.at(i)) = nullptr;
-                   break;
-               }
-           }
+        } else if (m_name.compare(Game::GAME_DAME) == 0) {
+            vector<int> travel = piece_dep->getTravel();
+            for (int i = 0; i < travel.size(); i++) {
+                if (m_board.at(travel.at(i)) != nullptr) {
+                    cout << "Les dames ca fait peur" << endl;
+                    m_board.at(travel.at(i)) = nullptr;
+                    break;
+                }
+            }
 
-            cout<<"Les dames c'est trop bien"<<endl;
+            cout << "Les dames c'est trop bien" << endl;
             m_board.at(y_dep * m_size + x_dep) = nullptr;
             m_board.at(y_arr * m_size + x_arr) = piece_dep;
-            ((GameDame*)this)->checkPawnTransform(x_arr, y_arr);
+            ((GameDame *) this)->checkPawnTransform(x_arr, y_arr);
         }
     }
 }
@@ -142,8 +142,8 @@ void Game::getTest(int idTest, std::string idBalise) {
         affichage();
         cout << endl << "endgame: " << m_endGame << endl;
         cout << vector[acc][0] << "," << vector[acc][1] << " -> "
-        << vector[acc][2] << "," << vector[acc][3] <<endl
-        <<" numCoup" << acc<<endl;
+             << vector[acc][2] << "," << vector[acc][3] << endl
+             << " numCoup" << acc << endl;
         acc++;
 
 
@@ -230,59 +230,59 @@ void Game::getTest(int idTest, std::string idBalise) {
 void Game::start() {
 
     cout << "Exemple de coup: 'A1A2' -> Piont A1 se deplace en A2 " << endl
-    << "Pour avoir la liste des coups d'une piece tapper (A1) : 'HELP A1' "<<endl;
-    int x_dep(-1),y_dep(-1),x_arr(-1),y_arr(-1),x_help(-1),y_help(-1);
+         << "Pour avoir la liste des coups d'une piece tapper (A1) : 'HELP A1' " << endl;
+    int x_dep(-1), y_dep(-1), x_arr(-1), y_arr(-1), x_help(-1), y_help(-1);
     string move("");
 
 
-        do{
-            cout << "*************************************************" << endl;
+    do {
+        cout << "*************************************************" << endl;
 
 
-            if (m_curP == WHITE)
-                cout << "Joueur: " << 1 << " a vous de jouer " << endl;
-            else
-                cout << "Joueur: " << 2 << " a vous de jouer " << endl;
-            affichage();
-            cout<<endl;
-            getline(cin, move);
-            cout << move.substr(0, 4)<<endl;
+        if (m_curP == WHITE)
+            cout << "Joueur: " << 1 << " a vous de jouer " << endl;
+        else
+            cout << "Joueur: " << 2 << " a vous de jouer " << endl;
+        affichage();
+        cout << endl;
+        getline(cin, move);
+        cout << move.substr(0, 4) << endl;
+        if (move.size() == 4 || move.size() == 7) {
 
             x_dep = (int) (move.at(0)) - 65;
             y_dep = (int) (move.at(1)) - 48;
             x_arr = (int) (move.at(2)) - 65;
             y_arr = (int) (move.at(3)) - 48;
 
-            if (move.substr(0, 4).compare("HELP") == 0){
-                if(move.size() == 7){
-                    x_help =(int) (move.at(5))-65;
-                    y_help = (int) (move.at(6))-48;
-                    getPossibleMove(x_help,y_help);
-                   /* cout << "HELP:"<<x_help<<y_help<<endl;
-                    cout << "HELP:"<<x_help<<y_help<< move.size()<<endl;*/
-
+            if (move.substr(0, 4).compare("HELP") == 0) {
+                if (move.size() == 7) {
+                    x_help = (int) (move.at(5)) - 65;
+                    y_help = (int) (move.at(6)) - 48;
+                    if (x_help < 0 || x_help >= m_size || y_help < 0 || y_help >= m_size)
+                        cout << "Hors limite recommencez" << endl;
+                    else
+                        getPossibleMove(x_help, y_help);
+                    /* cout << "HELP:"<<x_help<<y_help<<endl;
+                     cout << "HELP:"<<x_help<<y_help<< move.size()<<endl;*/
                 }
-            }
-            //cout << "AAAA :"<<x_dep<<y_dep<<x_dep<<y_arr<< move.size()<<endl;
-
-            else if(!movePiece(x_dep,y_dep,x_arr,y_arr)) {
+            } else if (!movePiece(x_dep, y_dep, x_arr, y_arr)) {
                 cout << "MOUVEMENT IMPOSSIBLE" << endl;
                 cout << "Exemple de coup: 'A1A2' -> Piont A1 se deplace en A2 " << endl;
                 cout << "Recommencez :" << endl;
-            }
-            else if(getColor(x_dep,y_dep) != m_curP){
+            } else if (getColor(x_dep, y_dep) != m_curP) {
                 cout << "Recommencez ce n'est pas votre couleur:" << endl;
-            }
-            else{
-                cout <<"OK" << endl;
-                Game::move(x_dep,y_dep,x_arr,y_arr);
+            } else {
+                cout << "OK" << endl;
+                Game::move(x_dep, y_dep, x_arr, y_arr);
                 if (m_curP == m_p1)
                     m_curP = m_p2;
                 else m_curP = m_p1;
             }
-        }while(!m_endGame);
-        cout<<"LA PARTIE EST FINIE"<<endl
-        <<"LE GAGNANT EST: "<< m_curP;
+        } else cout << "Pas Compris 2!" << endl;
+
+    } while (!m_endGame);
+    cout << "LA PARTIE EST FINIE" << endl
+         << "LE GAGNANT EST: " << m_curP;
 
 }
 
